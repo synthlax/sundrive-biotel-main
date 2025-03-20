b4b94c59b01f8048a24f47b1ed1bdc7b94906efd
synthlax <49820085+synthlax@users.noreply.github.com>
2025-03-19 21:24:57 -0500

    init: rust-backend setup

---
+++ server/target/debug/deps/server-cd7cef8447eacd20.d
@@ -0,0 +1,5 @@
+C:\Users\angel\Documents\GitHub\sundrive-biotel-main\server\target\debug\deps\libserver-cd7cef8447eacd20.rmeta: src\main.rs
+
+C:\Users\angel\Documents\GitHub\sundrive-biotel-main\server\target\debug\deps\server-cd7cef8447eacd20.d: src\main.rs
+
+src\main.rs:
